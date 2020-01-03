ea3921503636840056b6154ad515668335e204c1
Larry <tgfc045@tgfc.tw>
2020-01-03 17:44:22 +0800

    #021231 gfgfdgdf

--- src/main/java/com/example/demogit/Test.java
+++ src/main/java/com/example/demogit/Test.java
@@ -3,7 +3,10 @@ package com.example.demogit;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
 
 @RestController
 public class Test {
@@ -21,10 +24,11 @@ public class Test {
         Process p = null;
         String path = "C:\\project\\demo-git";
         try {
-            p = rt.exec("cmd /c dir cd "+path+" git log --online");
+            p = rt.exec("cmd /c dir cd "+path+" git log --oneline");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(p.toString());
+        BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
     }
 }
